94a2b4e500915c383e19362ed529ba9fcc2c803f
Hiran Ferreti Baccos <hiranfbcj@outlook.com>
2025-08-08 18:59:46 -0300

    first commit

---
+++ src/services/ContractService.java
@@ -0,0 +1,26 @@
+package services;
+
+import java.time.LocalDate;
+
+import entitites.Contract;
+import entitites.Installment;
+
+public class ContractService {
+	private OnlinePaymentService onlinePaymentService;
+
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+
+	// Cálculos (1% juro simples mensal + 2% taxa de pagamento)
+	public void processContract(Contract contract, int months) {
+		double basicQuota = contract.getTotalValue() / months;
+		for (int i = 1; i <= months; i++) {
+			LocalDate dueDate = contract.getDate().plusMonths(i);
+			double interest = onlinePaymentService.interest(basicQuota, i);
+			double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+			double quota = basicQuota + interest + fee;
+			contract.getInstallments().add(new Installment(dueDate, quota));
+		}
+	}
+}
